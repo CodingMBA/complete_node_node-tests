1508e47c2df1898af314b951b67fdbe702ecfc06
Andrew Smith <andrew@codingmba.com>
2018-03-05 15:54:35 -0500

    Initial commit

---
+++ utils/utils.test.js
@@ -0,0 +1,43 @@
+const expect = require('expect');
+
+const utils = require('./utils');
+
+it('should add two numbers', () => {
+    var result = utils.add(33, 11);
+
+    expect(result).toBe(44).toBeA('number');
+    // if (result !== 44) {
+    //     throw new Error(`Expected 44, but got ${result}.`)
+    // }
+});
+
+it('should square a number', () => {
+    var result = utils.square(4);
+    expect(result).toBe(16).toBeA('number');
+
+    // if (result !== 16) {
+    //     throw new Error(`Expected 44, but got ${result}.`)
+    // }
+});
+
+it('should include a firstName and lastName', () => {
+    var user =  {age: 51, location: 'Raleigh'};
+    var result = utils.setName(user, 'Andrew Smith');
+    // expect(user).toEqual(result);
+    expect(result).toInclude({ firstName: 'Andrew', lastName: 'Smith'}).toBeA('object');
+})
+
+// it('should expect some values',() => {
+//     // expect(12).toNotBe(11);
+//     // expect({name: 'Andrew'}).toBe({name: 'Andrew'})
+//     // expect({name: 'Andrew'}).toEqual({name: 'Andrew'})
+//     // expect([2,3,4]).toInclude(4);
+//     // expect([2,3,4]).toExclude(1);
+//     expect({
+//         name: 'Andrew',
+//         age: 51,
+//         location: 'Raleigh'
+//     }).toInclude({
+//         age: 51
+//     })
+// });
